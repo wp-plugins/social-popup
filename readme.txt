52e87483860a78312b4534c4222006fa16dfb789
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-11 20:48:42 +0000

    tagging version 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/social-popup/tags/1.1.1@597712 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Social PopUP ===
+Contributors: timersys
+Donate link: http://www.masquewordpress.com/
+Tags: twitter,google+, facebook,social popup, twitter follow, facebook like, google plus,social boost, social splash
+Requires at least: 3
+Tested up to: 3.3.2
+Stable tag: 1.1
+
+This plugin will display a popup or splash screen when a new user visit your site showing a Google+, twitter and facebook follow links. This will increase you followers ratio in a 40%. Popup will be close depending on your settings. 
+
+
+
+== Description ==
+
+This plugin will display a popup or splash screen when a new user visit your site showing a Google+, twitter and facebook follow links. This will increase you followers ratio in a 40%. Popup will be close depending on your settings. 
+
+Popup dissapears if a user click on any social button.
+
+Popup dissapears if a user click outside of the popup ( Can be disabled )
+
+Popup dissapears if a user click in close button ( Can be disabled )
+
+When a use clicks a button, a cookie will be created , so the user wont see the popup next time he visits the site.
+
+After you activate your plugin, go to the settings page to configure your buttons and enable the popup.
+
+You can also configure background opacity.
+
+
+= Plugin's Official Site =
+
+Social PopUP ([http://www.masquewordpress.com](http://www.masquewordpress.com/))
+
+= Increase your twitter followers  =
+
+Increase your Twitter followers with Twitter likebox Plugin ([http://wordpress.org/extend/plugins/twitter-like-box-reloaded/](http://wordpress.org/extend/plugins/twitter-like-box-reloaded/))
+
+== Installation ==
+
+1. Unzip and Upload the directory 'social-popup' to the '/wp-content/plugins/' directory
+
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+3. Go to Settings -> Social PopUP and configure the plugin
+
+
+== Changelog ==
+
+= 1.1 =
+* Added the ability to not show popup once is clicked
+
+= 1.0 =
+* First version
+
+
+== Screenshots ==
+
+1. Social PopUp
---
+++ spu.js
@@ -0,0 +1,102 @@
+(function($) {
+	
+	$.fn.socialPopUP = function(options) {
+		var defaults = { days_no_click : "10" };
+		var options = $.extend(defaults, options);
+		getPopHTML = function() {
+			var spClose = '';
+			if (defaults.closeable == true) {
+				spClose = '<a href="#" onClick="spuFlush('+defaults.days_no_click+');" id="spu-close">Close<a/>';
+			}
+			var sPop = '<div id="spu-bg"></div><div id="spu-main"><div id="spu-title">' + spClose + '' + defaults.title + '</div><div id="spu-msg-cont"><div id="spu-msg">' + defaults.message + '</div>';
+			var googlePop = '<div class="spu-button"><div class="g-plusone" data-callback="googleCB" data-action="share" data-annotation="bubble" data-height="24" data-href="' + defaults.go_url + '"></div></div>';
+			var facebookPop = '<div class="spu-button"><div id="fb-root"></div><fb:like href="' + defaults.fb_url + '" send="false"  show_faces="false" data-layout="button_count"></fb:like></div>';
+			var twitPop = '<div class="spu-button first"><a href="https://twitter.com/' + defaults.twitter_user + '" class="twitter-follow-button" data-show-count="false" data-size="large">Follow Me</a></div>';
+			var sCredits = '';
+			if (defaults.credits) sCredits = '<span style="font-size:10px;float: right;margin-top: -6px;">By <a href="http://www.masquewordpress.com">MasqueWordpress.com</a></span>';
+			var sBottom = '<div class="step-clear"></div></div><div id="spu-bottom">' + sCredits + '</div></div>';
+			return sPop + twitPop + facebookPop + googlePop + sBottom;
+		}
+		var markup = getPopHTML();
+		$('body').append(markup);
+		var cook = readCookie('spushow');
+		var waitCook = readCookie('spuwait');
+		if (cook != 'true' && waitCook != 'true') {
+			var windowWidth = document.documentElement.clientWidth;
+			var windowHeight = document.documentElement.clientHeight;
+			var popupHeight = $("#spu-main").height();
+			var popupWidth = $("#spu-main").width();
+			$("#spu-main").css({
+				"position": "absolute",
+				"top": 250,
+				"left": windowWidth / 2 - popupWidth / 2
+			});
+			$("#spu-bg").css({
+				"height": windowHeight
+			});
+			$("#spu-bg").css({
+				"opacity": defaults.opacity
+			});
+			$("#spu-bg").fadeIn("slow");
+			$("#spu-main").fadeIn("slow");
+		}
+		if (defaults.advancedClose == true) {
+			$(document).keyup(function(e) {
+				if (e.keyCode == 27) {
+					spuFlush(defaults.days_no_click);
+				}
+			});
+			$('body').click(function() {
+				spuFlush(defaults.days_no_click);
+			});
+			$('#spu-main').click(function(event) {
+				event.stopPropagation();
+			});
+		}
+		return true;
+	};
+})(jQuery);
+jQuery(document).ready(function(){
+FB.Event.subscribe('edge.create', function(href) {
+	spuFlush();
+});
+twttr.ready(function(twttr) {
+	twttr.events.bind('tweet', twitterCB);
+	twttr.events.bind('follow', twitterCB);
+});
+});
+function twitterCB(intent_event) {
+	spuFlush();
+}
+
+function googleCB() {
+	spuFlush();
+}
+
+function spuFlush( days = 99) {
+	
+	createCookie('spushow', 'true', days);
+	
+	jQuery("#spu-bg").fadeOut("slow");
+	jQuery("#spu-main").fadeOut("slow");
+}
+
+function createCookie(name, value, days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+	} else var expires = "";
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
