4d0ef35fae268baad47377eb63d363c24425a676
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-22 17:34:59 +0000

    small change and taggin correctly hopefully
    git-svn-id: https://plugins.svn.wordpress.org/social-popup/trunk@716776 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WP_Plugin_Base.class.php
+++ WP_Plugin_Base.class.php
@@ -23,7 +23,12 @@
 *	LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 *	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 *	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-****************************************************************************/
+****************************************************************************
+ * @author      Damian Logghe <info@timersys.com>
+ * @license     MIT License https://github.com/serbanghita/Mobile-Detect/blob/master/LICENSE.txt
+ * @link        GitHub Repository: https://github.com/timersys/wp-plugin-base
+ * @version     1.0
+ */
 
 /*
 * I also took quite lot of code from http://alisothegeek.com/2011/04/wordpress-settings-api-tutorial-follow-up/
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.timersys.com/
 Tags: twitter,google+, facebook,social popup, twitter follow, facebook like, google plus,social boost, social splash
 Requires at least: 3
 Tested up to: 3.5.1
-Stable tag: 1.5.1
+Stable tag: 1.5.2
 
 This plugin will display a popup or splash screen when a new user visit your site showing a Google+, twitter and facebook follow links. This will increase you followers ratio in a 40%. Popup will be close depending on your settings. 
 
