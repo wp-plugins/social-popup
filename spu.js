cc793bee60f8493d92b4102ac0a17e38fa157ebb
timersys <timersys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-31 17:11:28 +0000

    tagging version 1.6.4.3
    git-svn-id: https://plugins.svn.wordpress.org/social-popup/tags/1.6.4.3@848915 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- spu.js
+++ spu.js
@@ -1,7 +1,9 @@
-
+var spu_count = 0;
+var spu_counter ='';
 	function socialPopUP(options) {
 		var defaults = { days_no_click : "10" };
 		var options = jQuery.extend(defaults, options);
+		window.options = options;
 		
 		var cook = readCookie('spushow');
 		var waitCook = readCookie('spuwait');
@@ -13,7 +15,7 @@
 			var popupWidth = jQuery("#spu-main").width();
 			jQuery("#spu-main").css({
 				"position": "fixed",
-				"top": 250,
+				"top": windowHeight / 2 - popupHeight / 2,
 				"left": windowWidth / 2 - popupWidth / 2
 			});
 			jQuery("#spu-bg").css({
@@ -43,25 +45,56 @@
 				event.stopPropagation();
 			});
 		}
+		if( parseInt(defaults.s_to_close) > 0 )
+		{
+			spu_count=defaults.s_to_close;
+			spu_counter = setInterval(function(){spu_timer(defaults)}, 1000);
+		}
 		return true;
 	}
+
+function thanks_msg(options){
+
+	if( options.thanks_msg){
+		//I add some delay because twitter is not completing follow event
+		setTimeout(function(){
+			jQuery('#spu-msg-cont').hide().html(options.thanks_msg).fadeIn();
+		}, 500);
+	}
+	setTimeout(function(){ spuFlush()}, 1000 * options.thanks_sec);
+}
 
 
 jQuery(document).ready(function(){
 FB.Event.subscribe('edge.create', function(href) {
-	spuFlush();
+	clearInterval(spu_counter);
+	thanks_msg(window.options);
 });
-twttr.ready(function(twttr) {
-	twttr.events.bind('tweet', twitterCB);
-	twttr.events.bind('follow', twitterCB);
-});
+if (typeof twttr !== 'undefined') {
+	twttr.ready(function(twttr) {
+		clearInterval(spu_counter);
+		twttr.events.bind('tweet', twitterCB);
+		twttr.events.bind('follow', twitterCB);
+	});
+}
 });
 function twitterCB(intent_event) {
-	spuFlush();
+	thanks_msg(window.options);
 }
 
-function googleCB() {
-	spuFlush();
+function googleCB(a) {
+	clearInterval(spu_counter);
+	if( "on" == a.state )
+	{
+	 //	setTimeout(function(){thanks_msg(window.options)},2500);
+	}
+
+}
+function closeGoogle(a){
+	if( "confirm" == a.type )
+	{
+	setTimeout(function(){thanks_msg(window.options)},2500);
+	}
 }
 
 function spuFlush( days ) {
@@ -90,4 +123,16 @@ function readCookie(name) {
 		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
 	}
 	return null;
+}
+function spu_timer(defaults)
+{
+  spu_count=spu_count-1;
+  if (spu_count <= 0)
+  {
+     clearInterval(spu_counter);
+     spuFlush(defaults.days_no_click);
+     return;
+  }
+
+ jQuery("#spu-timer").html(defaults.esperar+" "+spu_count + " " + defaults.segundos);
 }
